8c238a493197881cfedb2311c6aca2ed4d9f56df
AlexanderRain <40913032+AlexanderRain@users.noreply.github.com>
2018-07-31 13:35:38 +0300

    Service rework
    Service rework

---
+++ app/src/main/java/com/example/b/activity/MyService.java
@@ -0,0 +1,58 @@
+package com.example.b.activity;
+
+import android.app.DownloadManager;
+import android.app.IntentService;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.widget.Toast;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class MyService extends IntentService {
+
+
+    public MyService() {
+        super("myname");
+    }
+
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        String url1 = intent.getStringExtra("url");
+        saveOnSDCard(url1);
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+    }
+    public void saveOnSDCard(String url) {
+        File dir = new File(Environment.getExternalStorageDirectory() + "/BIGDIG/test/B");
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+
+        String[] array = url.split("/");
+        url = array[array.length - 1];
+        url = url.substring(0, url.indexOf("."));
+
+        File file = new File(dir, url + ".png");
+
+        OutputStream os;
+        try {
+            os = new FileOutputStream(file);
+            os.flush();
+            os.close();
+        } catch (IOException ioe) {
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    ("Ошибка"+ioe.toString()), Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+}
